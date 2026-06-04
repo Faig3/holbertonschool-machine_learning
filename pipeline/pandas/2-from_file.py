01c68d132e03752940a60cae707344449554832a
Faig3 <13603@holbertonstudents.com>
2026-06-04 14:15:07 +0000

    2

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
-"doc"
+"""Module that loads data from a file as a pd.DataFrame."""
+import pandas as pd
+
 
 def from_file(filename, delimiter):
-    "doc"
-    return pd.read_csv(filename, delimiter)
+    """Load data from a file as a pd.DataFrame.
+
+    Args:
+        filename (str): The file to load from.
+        delimiter (str): The column separator.
+
+    Returns:
+        pd.DataFrame: The loaded DataFrame.
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
